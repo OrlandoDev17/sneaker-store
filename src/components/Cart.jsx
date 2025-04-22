3c344de682a863f7971eaca6d8f6ca634bb991d4
OrlandoDev17 <orlandodev1707@gmail.com>
2025-04-22 00:43:55 -0400

    feat: Integrar funcionalidades de agregar y eliminar productos en el catálogo de zapatos

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,18 +1,27 @@
 import { useId } from 'react';
 import { CartIcon } from '@/icons/icons';
 
+import { useStore } from '@nanostores/react';
+import { cartStore } from '@/stores/cartStore';
+
 export default function Cart() {
   const cartCheckboxId = useId();
+
+  const cart = useStore(cartStore);
 
   return (
     <>
       <div className="">
         <label
-          className="cart-button flex items-center justify-center"
+          className="cart-button relative flex items-center justify-center"
           htmlFor={cartCheckboxId}
         >
           <CartIcon className="size-14 border-2 p-2 rounded-full inline-block cursor-pointer hover:border-blue-500 hover:text-blue-500 hover:scale-110 transition" />
+          <span className="absolute top-0 -right-2 bg-red-500 text-white text-base font-semibold  px-2 rounded-full">
+            {cart.length}
+          </span>
         </label>
+
         <input type="checkbox" id={cartCheckboxId} hidden />
 
         <aside className="cart flex items-center justify-center absolute mt-5 h-full min-h-[90vh] w-100  right-0 z-10 bg-primary text-white"></aside>
--- src/components/ShoeCatalogue.jsx
+++ src/components/ShoeCatalogue.jsx
@@ -1,51 +1,76 @@
-import { PlusIcon } from '@/icons/icons';
-import { StarIcon } from '@/icons/icons';
-import { useCart } from '@/hooks/useCart';
+import { StarIcon, AddToCartIcon, RemoveFromCartIcon } from '@/icons/icons';
+
+import { useStore } from '@nanostores/react';
+import { cartStore, addToCart, RemoveFromCart } from '@/stores/cartStore';
 
 export default function ShoeCatalogue({ shoes }) {
-  // const { addToCart } = useCart();
+  const cart = useStore(cartStore);
+
+  const checkProductInCart = (shoe) => {
+    return cart.some((item) => item.id === shoe.id);
+  };
+
+  const handleCartAction = (event, shoe) => {
+    event.preventDefault();
+    if (checkProductInCart(shoe)) {
+      RemoveFromCart(shoe);
+    } else {
+      addToCart(shoe);
+    }
+  };
 
   return (
     <div className="grid xl:grid-cols-3 2xl:grid-cols-4 my-12 gap-4 md:gap-8">
-      {shoes.map((shoe) => (
-        <article className=" bg-light justify-center items-center p-6 rounded-2xl hover:-translate-y-4 hover:bg-primary group transition cursor-pointer">
-          <a className="flex flex-col gap-y-6" href={`/shoes/${shoe.id}`}>
-            <picture className="relative top-0 bg-white p-4 rounded-xl">
-              <span className="flex items-center gap-2 font-semibold text-base bg-light p-2 rounded-xl absolute">
-                {shoe.rating}
-                <StarIcon className="text-yellow-400" />
-              </span>
-              <img
-                className="h-64 object-contain"
-                src={shoe.images[0]}
-                alt={`Imagen del zapato ${shoe.name}`}
-                transitionName={`shoe-image-${shoe.id}`}
-              />
-            </picture>
-            <div className="flex flex-col gap-2">
-              <div className="flex justify-between">
-                <h2 className="font-semibold w-62 md:w-64 group-hover:text-white transition">
-                  {shoe.name}
-                </h2>
-                <button
-                  onClick={() => addToCart(shoe)}
-                  className="inline-flex rounded-full items-center justify-center"
-                >
-                  <PlusIcon className="bg-white size-12 p-2 rounded-full cursor-pointer" />
-                </button>
+      {shoes.map((shoe) => {
+        const isProductInCart = checkProductInCart(shoe);
+
+        return (
+          <article
+            key={shoe.id}
+            className=" bg-light justify-center items-center p-6 rounded-2xl hover:-translate-y-4 hover:bg-primary group transition cursor-pointer"
+          >
+            <a className="flex flex-col gap-y-6" href={`/shoes/${shoe.id}`}>
+              <picture className="relative top-0 bg-white p-4 rounded-xl">
+                <span className="flex items-center gap-2 font-semibold text-base bg-light p-2 rounded-xl absolute">
+                  {shoe.rating}
+                  <StarIcon className="text-yellow-400" />
+                </span>
+                <img
+                  className="h-64 object-contain"
+                  src={shoe.images[0]}
+                  alt={`Imagen del zapato ${shoe.name}`}
+                  transitionName={`shoe-image-${shoe.id}`}
+                />
+              </picture>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between">
+                  <h2 className="font-semibold w-62 md:w-64 group-hover:text-white transition">
+                    {shoe.name}
+                  </h2>
+                  <button
+                    onClick={(event) => handleCartAction(event, shoe)}
+                    className="add-to-cart relative inline-flex rounded-full items-center justify-center bg-white size-14 p-3 cursor-pointer hover:scale-125 transition"
+                  >
+                    {isProductInCart ? (
+                      <RemoveFromCartIcon className="size-10" />
+                    ) : (
+                      <AddToCartIcon className="size-10" />
+                    )}
+                  </button>
+                </div>
+                <div className="flex justify-between">
+                  <p className="text-lg font-rubik text-primary/60 group-hover:text-white transition">
+                    {shoe.maker}
+                  </p>
+                  <p className="text-xl tracking-widest font-rubik font-bold group-hover:text-white transition">
+                    ${shoe.price}.00
+                  </p>
+                </div>
               </div>
-              <div className="flex justify-between">
-                <p className="text-lg font-rubik text-primary/60 group-hover:text-white transition">
-                  {shoe.maker}
-                </p>
-                <p className="text-xl tracking-widest font-rubik font-bold group-hover:text-white transition">
-                  ${shoe.price}.00
-                </p>
-              </div>
-            </div>
-          </a>
-        </article>
-      ))}
+            </a>
+          </article>
+        );
+      })}
     </div>
   );
 }
