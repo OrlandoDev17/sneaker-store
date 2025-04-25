6dcda9a426b9dc9e3b6919a8155e52821696ed4c
OrlandoDev17 <orlandodev1707@gmail.com>
2025-04-24 22:48:51 -0400

    fix: corregir el uso de class a className en los iconos de agregar y eliminar del carrito

--- src/icons/icons.jsx
+++ src/icons/icons.jsx
@@ -111,7 +111,7 @@ export function AddToCartIcon({ className }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      class={`icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart-plus ${className}`}
+      className={`icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart-plus ${className}`}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
@@ -135,7 +135,7 @@ export function RemoveFromCartIcon({ className }) {
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class={`icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart-x ${className}`}
+      className={`icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart-x ${className}`}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
