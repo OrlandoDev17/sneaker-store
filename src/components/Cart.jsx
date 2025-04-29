316dcd609434e8b78f13fe5ea578a8e78e317e95
OrlandoDev17 <orlandodev1707@gmail.com>
2025-04-29 00:36:19 -0400

    feat: añadir icono de cierre y mejorar el diseño del carrito

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,5 @@
 import { useId, useEffect } from 'react';
-import { CartIcon, ClearCartIcon } from '@/icons/icons';
+import { CartIcon, ClearCartIcon, XIcon } from '@/icons/icons';
 import { CartItem } from './CartItem';
 
 import { cartStore, clearCart, initialCart } from '@/stores/cartStore';
@@ -47,7 +47,17 @@ export function Cart() {
       />
 
       {/* Contenedor del carrito */}
-      <aside className="cart flex flex-col absolute h-[89vh] w-100 top-25 right-0 z-10 bg-primary text-white">
+      <aside className="cart flex flex-col items-center p-4 absolute h-[89vh] w-100 top-25 right-0 z-10 bg-primary text-white">
+        <div className="flex items-center gap-2">
+          <label htmlFor={cartCheckboxId}>
+            <XIcon className="stroke-2 size-8 cursor-pointer hover:text-red-500 hover:scale-125 transition" />
+          </label>
+          <h2 className="text-2xl font-semibold font-rubik tracking-wide">
+            Carrito de Compras
+          </h2>
+          <CartIcon className="size-8 stroke-2" />
+        </div>
+
         <div className="flex flex-col h-full">
           {/* Contenedor con desplazamiento para los productos */}
           <div className="flex-1 overflow-y-auto p-8">
@@ -59,12 +69,12 @@ export function Cart() {
           </div>
 
           {/* Botón "Limpiar carrito" */}
-          <div className="flex items-center justify-center p-4 bg-primary-dark">
+          <div className="flex items-center justify-center py-4 bg-primary-dark">
             <button
-              className="flex items-center justify-center text-primary bg-light rounded-full p-2 cursor-pointer hover:scale-125 transition"
+              className="flex items-center justify-center border-2 border-white rounded-full p-2 cursor-pointer hover:bg-red-500 hover:scale-110 transition"
               onClick={clearCart}
             >
-              <ClearCartIcon className="size-12 mx-auto" />
+              <ClearCartIcon className="size-8 mx-auto" />
             </button>
           </div>
         </div>
