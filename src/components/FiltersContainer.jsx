b17e5683aad9ca972685911fb032c77f2165faa4
OrlandoDev17 <orlandodev1707@gmail.com>
2025-05-12 14:23:05 -0400

    refactor: ajustar clases de estilo en FiltersContainer y SearchBar para mejorar la presentación

--- src/components/FiltersContainer.jsx
+++ src/components/FiltersContainer.jsx
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar';
 
 export default function FiltersContainer({ changeFilters }) {
   return (
-    <div className="flex flex-col w-full md:flex-row items-center gap-2 justify-between mt-4">
+    <div className="flex flex-col w-full lg:flex-row items-center gap-2 justify-between mt-4">
       <FilterButtons onChange={changeFilters} />
       <SearchBar />
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,13 +8,13 @@ export default function SearchBar() {
   const searchId = useId();
 
   return (
-    <div className="flex items-center justify-center">
-      <form className="search-form relative flex items-center justify-center group">
+    <div className="flex items-center justify-center ">
+      <form className="relative flex items-center justify-center group gap-4 mt-8 ">
         <label className="relative cursor-pointer" htmlFor={searchId}>
-          <SearchIcon className="size-10 md:size-14 border-2 p-1 md:p-2 rounded-full inline-block group-hover:border-blue-500 group-hover:text-blue-500 group-hover:scale-110 transition" />
+          <SearchIcon className="size-10 md:size-14 border-2 p-1 md:p-2 rounded-full inline-block group-hover:border-blue-500 group-hover:text-blue-500 group-hover:scale-110 transition order-2" />
         </label>
         <input
-          className="absolute right-16 w-0 invisible bg-light py-3 px-2 rounded-2xl text-base font-semibold text-primary outline-none focus:ring-2 focus:ring-blue-500 group-hover:visible group-hover:w-56 transition-all"
+          className="lg:absolute right-16 lg:w-0 lg:invisible bg-light py-3 px-2 rounded-2xl text-base font-semibold text-primary outline-none focus:ring-2 focus:ring-blue-500 group-hover:visible group-hover:w-56 transition-all order-1"
           id={searchId}
           onChange={handleSearchCange}
           type="search"
